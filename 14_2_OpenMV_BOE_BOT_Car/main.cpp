bc18bbeef5bd913575b18b89c467ddb449044fbf
ht94103 <ee106061271@gapp.nthu.edu.tw>
2020-06-17 17:03:38 +0800

    Put OpenMV H7 plus on the BOE BOT Car

---
+++ 14_2_OpenMV_BOE_BOT_Car/main.cpp
@@ -0,0 +1,26 @@
+#include"mbed.h"
+
+
+Serial pc(USBTX,USBRX); //tx,rx
+
+Serial uart(D1,D0); //tx,rx
+
+
+int main(){
+
+   uart.baud(9600);
+   pc.printf("recv");
+
+   while(1){
+
+      if(uart.readable()){
+
+            char recv = uart.getc();
+
+            pc.putc(recv);
+
+      }
+
+   }
+
+}
